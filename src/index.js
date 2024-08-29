8b482b77613f6f4760495d625a28ec9a4675c9cd
Anton Usatiuk <160845263+UsaAnton@users.noreply.github.com>
2024-08-29 18:52:34 +0300

    feat

--- src/index.js
+++ src/index.js
@@ -38,7 +38,28 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let textArr = [];
+    for (let n = 0; n < expr.length; n += 10) {
+        let charArr = [];
+        for (let i = n; i < n + 10; i += 2) {
+            if (expr[i] === "1" && expr[i + 1] === "0") {
+                charArr.push(".")
+            }
+            if (expr[i] === "1" && expr[i + 1] === "1") {
+                charArr.push("-")
+            }
+            if (expr[i] === "*") {
+                textArr.push(" ")
+                break;
+            }
+        }
+        for (let char in MORSE_TABLE) {
+            if (charArr.join('') === char) {
+                textArr.push(MORSE_TABLE[char])
+            }
+        }
+    }
+    return textArr.join('');
 }
 
 module.exports = {
